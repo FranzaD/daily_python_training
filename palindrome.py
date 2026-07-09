1d654b371b166cb98390e208f89c094d72908d99
FranzaD <deborah.r.franza@gmail.com>
2026-07-08 21:03:06 -0700

    Writing out initial operations and logic for solution, will properly draft code tomorrow.

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+# Leetcode Question 2: Palindrome
+# Solved 
+# Big O Notation: O() time
+# Easy
+# https://leetcode.com/problems/palindrome-number/
+
+# reviewed modulo operation, also learned about the floor division operator // which returns the largest integer less than or equal to the result of the division.
+# I also determined that subtracting the modulo from the result and dividing by 10 is another way of getting the floor of the division of a number by 10, which is useful for removing the last digit of a number.
+# now tomorrow I'm going to determine how to use these operations in a loop to compare digits for determining if a number is a palindrome.
