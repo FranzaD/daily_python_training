dd0bb2251ea2287427d64d1e65ea4a3c73cad585
FranzaD <deborah.r.franza@gmail.com>
2026-07-09 21:42:04 -0700

    main logic using modulo and floor function with positive integers is solved. Now need to work on logic accounting for negative integers since it changes the math!

--- palindrome.py
+++ palindrome.py
@@ -7,3 +7,34 @@
 # reviewed modulo operation, also learned about the floor division operator // which returns the largest integer less than or equal to the result of the division.
 # I also determined that subtracting the modulo from the result and dividing by 10 is another way of getting the floor of the division of a number by 10, which is useful for removing the last digit of a number.
 # now tomorrow I'm going to determine how to use these operations in a loop to compare digits for determining if a number is a palindrome.
+
+#still have yet to handle the case when x is negative!
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        #declares both arrays for usage
+        original_seq = []
+        reversed_seq = []
+
+        # reverse the number, store in array for comparison
+        while True:
+                last_digit = x % 10
+                x = x//10
+                reversed_seq.append(last_digit)
+                if x == 0:
+                    break 
+
+        #copy over reversed array
+        original_seq = list(reversed_seq)
+
+        # reverses the reversed array so original order is re-established
+        original_seq.reverse()
+
+        #now conduct comparison
+        if original_seq == reversed_seq:
+            return True
+        
+        return False 
