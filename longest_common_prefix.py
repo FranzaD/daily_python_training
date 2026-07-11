65bd7419c2b0adfb7034600b9050f3e5676ed68a
FranzaD <deborah.r.franza@gmail.com>
2026-07-11 11:53:41 -0700

    Initial attempt, not solved

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,5 +1,36 @@
 # Leetcode Question 4: Longest Common Prefix
 # Solved: 
-# Big O Notation: O(n) runtime
+# Big O Notation: O() runtime
 # Easy
-# https://leetcode.com/problems/roman-to-integer/
+# https://leetcode.com/problems/longest-common-prefix/description/
+
+#initial attempt, overcomplicated logic
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        temp_common_longest_prefix = []
+        longest_common_prefix = []
+
+        # first string gets copied over to new list to compare all other strings to it
+        longest_common_prefix = [strs[0]]
+
+        #iterate through each string string in the array of strings
+        for string in strs:
+            # need to make sure it does run through checking the first string against itself
+            if common_longest_prefix == strs[0]:
+                #skip the following for loop since no need to check the same value against itself
+                continue 
+
+                # so this for loop should start when string = 1 and char_index = 0
+                # checks the characters against eachother from the list to the array of lists
+                for char_index in longest_common_prefix: 
+
+                    # compare characters from one string to another
+                    if longest_common_prefix[char_index] != string[char_index]:
+                        temp_common_longest_prefix = longest_common_prefix[:char_index]
+
+        return temp_common_longest_prefix
