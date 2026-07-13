6264965863fc3dbe16c57b2526459cfcae8a81e9
FranzaD <deborah.r.franza@gmail.com>
2026-07-13 11:18:54 -0700

    Final solution achieved by altering logic one step at a time and making sure I'm not destroying logic that helped other test cases run.
    Went overtime to solve without the feedback from Claude. First time flying solo in the end to achieve a solution where all test cases passed!

--- valid_parantheses.py
+++ valid_parantheses.py
@@ -12,6 +12,9 @@
     # adding happens in the back
     # removing happens at the front
 
+# Learned that I don't need to rethink the whole logic, just try to add the simpliest solution first and build little by little fo reach test case that fails.
+# first leetcode solution I troubleshot without feedback from Claude! Took longer than expected but working through each failed test case and rerunning the code to see which cases passed help me stay on track
+# one change in the logic = rerun test cases again. Don't change a bunch of logic at once, just one change at a time and rerun test cases to see if it passes or fails. If it fails, then I know the change I made is wrong and I can revert back to the previous version of the code.
 class Solution(object):
     def isValid(self, s):
         """
@@ -39,13 +42,12 @@ class Solution(object):
                     # is the last element in stack an open bracket? if so then pop off stack
                     if stack[-1] ==  dict[bracket]:
                         stack.pop()
-                
-                # if the stack is empty, then throw the closing bracket in anyway
-                if stack == [] and len(s) == 1:
-                    stack.append(bracket)
-                # when a closing bracket exists and the stack is empty
-                #if stack == []:
-                #    return False
+                    # bracket mismatch
+                    else: 
+                        return False 
+                # closing bracket and stack is empty
+                else: 
+                    return False 
 
         #if all parantheses had pairs by the end of the for loop it should be empty, otherwise false
         if stack == []:
