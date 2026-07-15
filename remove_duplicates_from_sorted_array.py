aa3e0b234ebb4da66a4be9642c09b57d73e350c0
FranzaD <deborah.r.franza@gmail.com>
2026-07-15 09:04:52 -0700

    Setting up question for tomorrow, logic still be written on paper

---
+++ remove_duplicates_from_sorted_array.py
@@ -0,0 +1,12 @@
+# Leetcode Question 7: Remove Duplicates from Sorted Array
+# Solved:
+# Big O Notation: O() runtime 
+# Easy
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
