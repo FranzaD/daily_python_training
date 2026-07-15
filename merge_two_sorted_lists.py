465d98070024b61027b12d64a08df2e1a57c202e
FranzaD <deborah.r.franza@gmail.com>
2026-07-15 08:40:11 -0700

    Functional logic achieved, passes all test cases.

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -1,10 +1,11 @@
 # Leetcode Question 6: Merge Two Sorted Lists
-# Solved: 
+# Solved: 7/15/2026
 # Big O Notation: O() runtime 
 # Easy
 # https://leetcode.com/problems/merge-two-sorted-lists/description/
 
 
+# Definition for singly-linked list.
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, val=0, next=None):
@@ -20,25 +21,40 @@ class Solution(object):
         """
         current1 = list1
         current2 = list2
-        list3 = list1
+        list3 = ListNode()
+        tail = list3
 
         while current1 is not None and current2 is not None:
-            # if values are equal chain to list3
-            if current1.value == current2.value:
-                #move current to next node
+            # if values are equal default to chaining list1 into list 3 first then list2
+            if current1.val == current2.val:
+                tail.next = current1
+                tail = tail.next
                 current1 = current1.next
-                #attache node to equal node
-                list3.next = current2
-                
-                current2.next = current1.next
-                current1.next = current2
-                
+                tail.next = current2
+                tail = tail.next
+                current2 = current2.next
+
+            # if list 1 value is less than list 2 value
+            elif current1.val < current2.val:
+                tail.next = current1
+                tail = tail.next
+                current1 = current1.next
             
-            if current1.value > current2.value:
-                current2.next = current1
+            # if list 1 value is greater than list 2 value
+            elif current1.val > current2.val:
+                tail.next = current2
+                tail = tail.next
+                current2 = current2.next
+        
+        # if there is a list with remaining nodes then since it's already order I can just append it to the end of list 3
+        if current1 is None:
+            tail.next = current2
 
-            if current1.value < current2.value:
-                current1.next = current2
+        if current2 is None:
+            tail.next = current1
 
-        current1 = list1.next
-        current2 = list2.next
+        # now that lists are spliced together in order then delete the dummy node
+        # so output doesn't include a leading null element
+        tail = list3
+        list3 = tail.next
+        return list3
