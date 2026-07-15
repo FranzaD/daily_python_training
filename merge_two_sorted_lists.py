be9a6c6eb0ee8e628263c7c61ba7cc4975aa38cb
FranzaD <deborah.r.franza@gmail.com>
2026-07-14 19:55:17 -0700

    First attempt of using pointers in python, logic isn't quite right. Need to figure out how many poitners are necessary to solve this problem (so far 3 hasn't been enough)

---
+++ merge_two_sorted_lists.py
@@ -0,0 +1,44 @@
+# Leetcode Question 6: Merge T
+# Solved: 
+# Big O Notation: O() runtime 
+# Easy
+# https://leetcode.com/problems/longest-common-prefix/description/
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        current1 = list1
+        current2 = list2
+        list3 = list1
+
+        while current1 is not None and current2 is not None:
+            # if values are equal chain to list3
+            if current1.value == current2.value:
+                #move current to next node
+                current1 = current1.next
+                #attache node to equal node
+                list3.next = current2
+                
+                current2.next = current1.next
+                current1.next = current2
+                
+            
+            if current1.value > current2.value:
+                current2.next = current1
+
+            if current1.value < current2.value:
+                current1.next = current2
+
+        current1 = list1.next
+        current2 = list2.next
