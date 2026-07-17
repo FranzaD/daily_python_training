df28e06f3b4cd71bc4f9f61e5c5eb95824dfafb2
FranzaD <deborah.r.franza@gmail.com>
2026-07-17 16:21:15 -0700

    Drafting logic on paper for next question, will code up tomorrow.

---
+++ remove_element.py
@@ -0,0 +1,16 @@
+# Leetcode Question 8: Remove Element
+# Solved:
+# Big O Notation: O() runtime ()
+# Easy
+# https://leetcode.com/problems/remove-element/description/
+
+# Learned:
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
