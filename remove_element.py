3457c00de1e62a1c13fbf31657bbbaa108c3e880
FranzaD <deborah.r.franza@gmail.com>
2026-07-18 21:09:43 -0700

    Solved in O(n) time. used write as a counter and pointer for the nums array and simplified logic using an if-else.

--- remove_element.py
+++ remove_element.py
@@ -1,10 +1,10 @@
 # Leetcode Question 8: Remove Element
-# Solved:
-# Big O Notation: O() runtime ()
+# Solved: 7/18/2026
+# Big O Notation: O(n) runtime 
 # Easy
 # https://leetcode.com/problems/remove-element/description/
 
-# Learned:
+# Learned: that I can use an simple else statement if there is only one other alternative option that I'm checking
 
 class Solution(object):
     def removeElement(self, nums, val):
@@ -13,4 +13,17 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        read = 0
+        write = 0
+
+        while read != len(nums):
+            if nums[read] == val:
+                read+=1
+            #elif nums[read] != val:
+            else:
+                nums[write] = nums[read]
+                read +=1
+                write +=1
         
+        return write
+            
