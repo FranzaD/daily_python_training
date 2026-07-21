853f2bc13f3cddd6b97eb6e359c1b61030671a49
FranzaD <deborah.r.franza@gmail.com>
2026-07-21 12:57:50 -0700

    Draft of binary search logic, not complete, need to refine since there are many cases it doesnt pass.

--- search_insert_position.py
+++ search_insert_position.py
@@ -22,3 +22,26 @@ class Solution(object):
                 return len(nums)
             elif nums[i-1] < target < nums[i+1]:
                 return i
+
+        # solution that implements binary search
+        high_index = len(nums)-1
+        low_index = 0
+
+        # but low_index would never be higher than high_index
+        # the other thing I was thinking was using a flag, while glag = false, but then again, I can just break the loop by returning where the index matches the target no need to flag.
+        while low_index < high_index:
+            mid_index = (low_index + high_index)//2
+            if nums[mid_index] == target:
+                return mid_index
+            elif nums[mid_index] > target:
+                high_index = mid_index-1
+            elif nums[mid_index] < target:
+                low_index = mid_index+1
+        
+        # now low_index broke out of loop since low_index > high_index
+        # but then just return the index
+        if low_index > high_index:
+            return low_index
+
+        if low_index < high_index:
+            return high_index
