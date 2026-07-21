e36f314fc200ed3e0391eae7889fe96acc594cf6
FranzaD <deborah.r.franza@gmail.com>
2026-07-20 22:35:00 -0700

    Alterted outer loop range to eliminate redundant iterations and introduce a flag to simplify the if statement logic in the inbedded loop.

--- find_the_index_of_the_first_occurrence_in_a_string.py
+++ find_the_index_of_the_first_occurrence_in_a_string.py
@@ -1,10 +1,10 @@
 # Leetcode Question 9: Find the Index of the First Occurrence in a String
 # Solved: 
-# Big O Notation: O() runtime 
+# Big O Notation: O(n*m) runtime when n is the outer loop and m is the inner loop input 
 # Easy
 # https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
 
-# Learned:
+# Learned: Reminded of the use of flags and altering the range for a for loop to maximize efficiency
 
 class Solution(object):
     def strStr(self, haystack, needle):
@@ -13,11 +13,12 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        # iterate through each character in string
-        for char1 in haystack:
-            # iterate through each 
-            for char2 in needle:
-                if char1 == char2:
-                    return char1
-        
+        for i in range(len(haystack)-len(needle)+1):
+            index_found = True
+            for j in range(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    index_found = False
+                    break
+            if index_found:
+                return i
         return -1
