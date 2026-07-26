d0deb34716c9e1d14b65a5134b81348443f7e50c
FranzaD <deborah.r.franza@gmail.com>
2026-07-26 08:46:49 -0700

    Final solution with all test cases passed in O(n) runtime. Leveraged iterating backwareds through strings to mimic binary addition and appended leading zeros to smaller string to avoid index error. Manually wrote out logic for binary addition with the limitation that strings are immutable in python.

--- add_binary.py
+++ add_binary.py
@@ -1,11 +1,10 @@
 # Leetcode Question 13: Add Binary
-# Solved: 
-# Big O Notation: O() runtime  
+# Solved: 7/26/2026
+# Big O Notation: O(n) runtime  
 # Easy
 # https://leetcode.com/problems/add-binary/description/
 
-# Learned: reminded that string are immutable in python!
-
+# Learned: reminded that string are immutable in python! Also refreshed binary addition logic and was reminded that strings can be initialized as empty (without a space)
 class Solution(object):
     def addBinary(self, a, b):
         """
@@ -14,34 +13,52 @@ class Solution(object):
         :rtype: str
         """
         # result string starts with blank space that can be trimmed off at the end with splice()
-        result = " "
-        carry_digit = 0
-        total_sum = 0
+        result = ""
+        carry = 0
+        total = 0
 
-        # loop that will compare characters in string a and b and a carry digit and correctly format the result string that will br returned.
-        for i in range(-1, -len(result), -1):
-            #sum characters in strings...but they're characters, not digits.
-            # calculate total
+        # get strings to be the same size so I dont get an index error in the loop when accessing an index teh smaller string doesn't have
+        #determine how many zeros characters to append to the smaller string and append
+        size_difference = abs(len(a)-len(b))
+        zeros_substring = "0" * size_difference
+        if len(a) < len(b):
+            a = zeros_substring + a
+        else:
+            b = zeros_substring + b
+
+        # loop that will compare characters in string a and b
+        for i in range(-1, -(len(a)+1), -1):
+            # convert strings into integers to calculate total that dictates behavior
             if a[i] == "1":
                 total += 1
             if  b[i] == "1":
                 total += 1
             if carry == 1:
                 total += 1
-
-            # a and b are alternating
+            
+            # inserts correct character into result string based on total
+            # indexing result is actually unnecessary (and not possible), appending is enough
             if total == 0:
-                result[i] = "0"
-            #
+                result = "0" + result
             if total == 1:
-                result[i] = "1"
+                result = "1" + result
+                # after insertion carry can go back to 0
+                carry = 0
             if total == 2:
-                result[i] = "1"
+                result = "0" + result
                 carry = 1
             if total == 3:
-                result[i] = "1"
+                result = "1" + result
                 carry = 1
-                # but when should carry be reset?
+                # but when should carry be reset? When total = 1 it can be reset
 
-        return result
+            # reset total for the next comparison(iteration of the loop)
+            total = 0
 
+        # loop exited, only thing left to add to the front would be the carry if it has a 1
+        if carry == 1:
+            result = "1" + result
+            return result
+        else:
+            # carry = 0, just return result string
+            return result
