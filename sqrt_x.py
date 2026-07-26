b501fb3a8022afe1500af6d62a7d7fa021cffb85
FranzaD <deborah.r.franza@gmail.com>
2026-07-26 09:59:56 -0700

    Still writing out logic for solution on paper. Will reapproach tomorrow.

---
+++ sqrt_x.py
@@ -0,0 +1,9 @@
+# Leetcode Question 14: Sqrt(x)
+# Solved: 
+# Big O Notation: O() runtime  
+# Easy
+# https://leetcode.com/problems/sqrtx/description/
+
+# Learned:
+
+# reduce number down to it's least common denominators, extract pairs, if a prime is left then multiply extracted multiples by 
