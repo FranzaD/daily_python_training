a75494844005264ed05def86d5b74b951daa9c4e
FranzaD <deborah.r.franza@gmail.com>
2026-07-27 10:32:42 -0700

    FInal solution passes all test cases. Updated loop condition to more accurately represent the conditions of when the square was found. Binary search results in a runtime of O(logn)

--- sqrt_x.py
+++ sqrt_x.py
@@ -1,10 +1,10 @@
 # Leetcode Question 14: Sqrt(x)
-# Solved: 
-# Big O Notation: O() runtime  
+# Solved: 7/27/2026
+# Big O Notation: O(log n) runtime since binary search is used here
 # Easy
 # https://leetcode.com/problems/sqrtx/description/
 
-# Learned:
+# Learned: refresher on binary search. floor division, and refining bounds of a loop for precision
 
 # solution passes some test cases but not most cases
 class Solution(object):
@@ -15,24 +15,25 @@ class Solution(object):
         """
         low = 0
         high = x
+        #use floor operator since we need an integer and we cant assume provided x is a perfect square
         mid = (low + high)//2
-        previous_value = 0
 
-        # loop until x is equal to the mid value squared, loop ends only when square root has been found
-        while mid*mid != x and previous_value != mid:
+        # iterate until 
+        while low <= high:
+            if mid*mid == x:
+                return mid
             # if mid overshot x then high needs to be adjusted down
-            if mid*mid > x:
-                high = mid
+            elif mid*mid > x:
+                # we need to adjust the high value so it's slightly lower than mid
+                high = mid - 1
                 # ensure the mid is an integer with floor division
                 mid = (low + high)//2
             # if mid undershot x then low needs to be adjusted up
             elif mid*mid < x:
-                low = mid
+                # we need to adjust the low value so it's slightly higher than mid
+                low = mid + 1
                 # ensure the mid is an integer with floor division
                 mid = (low + high)//2
-
-            # if close enough integer found the loop convergences on a solution into infinity so I need to stop it if the value found in the former iteration = value in the current iteration
-            previous_value = mid
 
         # mid must be the square root if loop is finished
         return mid
