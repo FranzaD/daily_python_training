e1a6aa8accebb0f2239eef68c3e2d99d24326112
FranzaD <deborah.r.franza@gmail.com>
2026-08-03 09:55:13 -0700

    traced through edge cases to confirm that logic is sound, recursive solution achieved by taking into account base cases and comparing nodes with a helper function.

--- symmetric_tree.py
+++ symmetric_tree.py
@@ -1,10 +1,11 @@
 # Leetcode Question 19: Symmetric Tree
-# Solved: 
-# Big O Notation: O() runtime 
+# Solved: 8/3/2026
+# Big O Notation: O(n) runtime since it runs through every node in the tree not skipping any until found false
 # Easy
 # https://leetcode.com/problems/symmetric-tree/description/
 
-# Learned:
+# Learned: That recursion doesn't need to return a values to store in a list, instead nodes can be compared, 
+# and then iterated to the next with a new function call
 
 # Definition for a binary tree node.
 class TreeNode(object):
@@ -19,21 +20,21 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: bool
         """
-        def isMirror(p, q):
-            # if left and right subtrees are none, not symmetric since there's no tree
-            if p is None and q is None:
-                return False
-            # return if only one subtree is none
-            elif p is None and q is not None:
+        def isMirror(left_node, right_node):
+            # if left and right subtrees are none, symmetric since a node exists but left and right branches are none!
+            if left_node is None and right_node is None:
+                return True
+            # not symmetric if one branch is none and the other isn't
+            elif left_node is None and right_node is not None or left_node is not None and right_node is None:
                 return False
             # if values of left and right subtree nodes don't match 
-            elif p.val != q.val:
+            elif left_node.val != right_node.val:
                 return False
             # if the values of left and right subtree nodes do match continue traversing
-            elif p.val == q.val:
-                return isMirror(p.left, q.right) and isMirror(p.right, p.left)
+            elif left_node.val == right_node.val:
+                return isMirror(left_node.left, right_node.right) and isMirror(left_node.right, right_node.left)
             
         if root is None:
             return False
-        else:
+        else: # start recursion if root has node
             return isMirror(root.left, root.right)
