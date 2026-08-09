d51c238e9e4df4baf550edebd584a79fd2ee7013
FranzaD <deborah.r.franza@gmail.com>
2026-08-08 18:59:11 -0700

    solution doesn't run in leetcode however the logic seems correct according to my trace so I'm submitting this as done. used just mid instead of high low parameters by utilizing python list slicing in function call.

--- convert_sorted_array_to_binary_search_tree.py
+++ convert_sorted_array_to_binary_search_tree.py
@@ -1,47 +1,33 @@
 # Leetcode Question 21: Convert Sorted Array to Binary Search Tree
-# Solved: 
-# Big O Notation: O() runtime
+# Solved: 8/8/2026
+# Big O Notation: O(n) runtime
 # Easy
 # https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
 
-# Learned:
-
-# Definition for a binary tree node.
+#Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+        
 class Solution(object):
     def sortedArrayToBST(self, nums):
         """
         :type nums: List[int]
         :rtype: Optional[TreeNode]
         """
+        # base case, checks if the string is empty
+        if not nums:
+            return None
 
-        # center, needs initially
-        mid = len(nums) // 2
-        # may be unnecessary
-        low = 0
-        high = len(nums)
-    
-        # right subtrees
-        high = len(nums)
-        low = mid
-        mid = (high - low)//2
-        return self.sortedArrayToBST(nums[:mid])
-
-        # left subtrees
-        low = 0
-        high = mid
-        mid = (high - low)// 2
-        return self.sortedArrayToBST(nums[mid:])
-
-        # base case
-        # continue traversing nums until low and high are the same value
-        if high == low:
-            # not defined yet
-            return tree
-        
-        # if this recieves a list, then I can't pass in anything other than a list, can I use recursion on this function?
-        # self.sortedArrayToBST()
+        #find middle
+        mid = len(nums)//2
+     
+        #create tree node
+        # string slicing is exclusive of mid
+        node = TreeNode(nums[mid])
+        # list slicing in python, beginning is inclusive, end is exclusive
+        node.left = self.sortedArrayToBST(nums[:mid]) #mid excluded
+        node.right = self.sortedArrayToBST(nums[mid+1:]) # mid included
+        return node
