89b6cc74d58afdc01c357251469547d68e4e1d3a
FranzaD <deborah.r.franza@gmail.com>
2026-08-13 11:46:30 -0700

    All test cases passed leveraging a nested loop instead of recursion. Needed to trace through numRows = 5 in order to generalize the proper indexing and bounds.

--- pascals_traingle.py
+++ pascals_traingle.py
@@ -1,10 +1,12 @@
 # Leetcode Question 25: Pascal's Triangle
-# Solved: 
-# Big O Notation: O() runtime,
+# Solved: 8/13/2026
+# Big O Notation: O(n) runtime,
 # Easy
 # https://leetcode.com/problems/pascals-triangle/
 
-# Learned:
+# Learned: It is worth tracing through the first set of cases and looking for a pattern to determine proper ranges for indices and loops
+# if not enough rows are traced through the overall pattern is diifficult to get right
+# opt for using loops if possible before jumping to recursion
 
 class Solution(object):
     def generate(self, numRows):
@@ -12,27 +14,21 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
+        p_triangle = [[1],[1,1]]
+        
         # base cases
         if numRows == 1:
-            return [[1]]
+            return [p_triangle[0]]
         if numRows == 2:
-            return [[1],[1,1]]
+            return p_triangle
 
-        # call a function to create the next row necessary
-        # say numRows is 5, but we have only the first 2 rows, so then I need to build up to 5
-        # so 5-2 = 3, (numRows - 2) gets passed into the function?
-        starting_row = numRows-2
-
-        self.generate(starting_row):
-            new_row = [1]*starting_row
-            p_triangle.append(starting_row)
-
-            for i in range(1, starting_row):
-                p_triangle[starting_row][i] = p_triangle[starting_row-1][i-1] + p_triangle[starting_row-1][i] 
-            if starting_row == numRows:
-                return p_triangle
-            else:
-                self.generate(starting_row+1)
-        # but I need to build up to 5, so then I need to build row 3, row 4, and then row 5. I can't jsut jump into row 5
-
-        #end when starting row = numRow
+        #creates the new row intializing the correct number of elements as 1s, +1 since end is exclusive
+        for row in range(2, numRows):
+            #creates new row, initializing all values as one and then appends it to pascals triangle
+            p_triangle.append([1]*(row+1))
+            
+            #iterates through each index that who's value needs to be updated in current row
+            for i in range(0, row-1):
+                p_triangle[row][i+1]=p_triangle[row-1][i]+p_triangle[row-1][i+1]
+        
+        return p_triangle
